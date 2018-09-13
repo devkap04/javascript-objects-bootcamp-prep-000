4132c6d97f5f51d1d1708d4e1d255c5fc51ed8ce
devinkapelanski@yahoo.com <devinkapelanski@yahoo.com>
2018-09-13 05:18:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artist: 'song'}
 
-function updatePlaylist(playlist, song, artist) {
-  return Object.assign({}, playlist, {[song]: artist})
+function updatePlaylist(playlist, artistNanme, songTitle) {
+  return Object.assign({}, playlist, {[songTitle]: artistName})
 }
