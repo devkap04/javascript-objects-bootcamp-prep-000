f751c1d361d17de25def2f7536e1ae32c0811c17
devinkapelanski@yahoo.com <devinkapelanski@yahoo.com>
2018-09-13 05:34:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artist: 'song'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlistartistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist
 }
